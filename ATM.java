3c5ec1584c8b2894884a0a674cd5bfca16961c54
Cade Shiveley <cade_shiveley@mines.edu>
2025-10-06 15:10:50 -0600

    adding try's to the ATM class

---
+++ ATM.java
@@ -0,0 +1,19 @@
+public class ATM{
+    account = new BankAccount(500);
+
+    public void handleWithdraw(){
+        try{
+            //withdraws normal 600 not quick
+            normal.withdraw(600);
+            //if 
+        }
+        
+        try{
+            //withdraws quick 600
+            quick.quickWithdraw(600);
+        }
+
+    }
+
+
+}
