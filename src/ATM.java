747262164562add919a2c0940c742f96295876a9
Cade Shiveley <cade_shiveley@mines.edu>
2025-10-06 17:16:08 -0600

    adding to ATM and fixing syntax errors

--- src/ATM.java
+++ src/ATM.java
@@ -1,3 +1,26 @@
 public class ATM{
-    private BankAccount account;
+    account = new BankAccount(500);
 
+    public void handleWithdraw(){
+        try{
+            //withdraws normal 600 not quick
+            normal.withdraw(600);
+            //if 
+        }catch(NegativeBalanceException e){
+            System.out.println(e);
+            System.out.println(e.getMessage());
+        }
+
+        }
+        
+        try{
+            //withdraws quick 600
+            quick.quickWithdraw(600);
+        }catch(NegativeBalanceException e){
+            System.out.println(e);
+            System.out.println(e.getMessage());
+
+    }
+
+
+}
