86dfbb7c70d414e4a2e4664898f0de3926ccfc2a
Cade Shiveley <cade_shiveley@mines.edu>
2025-10-06 15:15:44 -0600

    adding catch with system.out.println's

--- ATM.java
+++ ATM.java
@@ -6,12 +6,19 @@ public class ATM{
             //withdraws normal 600 not quick
             normal.withdraw(600);
             //if 
+        }catch{
+            System.out.pintln(e)
+            System.out.println(e.getMessage())
+        }
+
         }
         
         try{
             //withdraws quick 600
             quick.quickWithdraw(600);
-        }
+        }catch{
+            System.out.pintln(e)
+            System.out.println(e.getMessage())
 
     }
 
