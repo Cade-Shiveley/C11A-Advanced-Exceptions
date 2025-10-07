2fb1d6d3b2ba9db3c1de8fdec065f9b2cac582ff
LukeBBell <LBell_@outlook.com>
2025-10-06 19:03:38 -0600

    Fixed ATM.java withdraw errors + added main

--- src/ATM.java
+++ src/ATM.java
@@ -1,26 +1,27 @@
 public class ATM{
     BankAccount account = new BankAccount(500);
 
-    public void handleWithdraw(){
+    public void handleTransactions(){
         try{
             //withdraws normal 600 not quick
-            normal.withdraw(600);
+            account.withdraw(600);
             //if 
         }catch(NegativeBalanceException e){
             System.out.println(e);
             System.out.println(e.getMessage());
         }
-
-        }
-        
         try{
             //withdraws quick 600
-            quick.quickWithdraw(600);
+            account.quickWithdraw(600);
         }catch(NegativeBalanceException e){
             System.out.println(e);
             System.out.println(e.getMessage());
 
+        }
     }
-
+public static void main(String[] args) {
+        ATM atm = new ATM();
+        atm.handleTransactions();
+    }
 
 }
