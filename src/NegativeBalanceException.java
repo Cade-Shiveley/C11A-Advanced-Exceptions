446000057654772e169ce3c5a45b51b613de0785
LukeBBell <LBell_@outlook.com>
2025-10-06 18:54:19 -0600

    filled out rest of exception class with constructors

--- src/NegativeBalanceException.java
+++ src/NegativeBalanceException.java
@@ -1,6 +1,23 @@
-public class NegativeBalanceException extends Exception{
-    public NegativeBalanceException(double negativeBalance){
-        super(negativeBalance);
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+public class NegativeBalanceException extends Exception {
+    private double negativeBalance;
+
+    public NegativeBalanceException() {
+        super("Error: negative balance");
+        this.negativeBalance = 0.0;
+    }
+
+    public NegativeBalanceException(double negativeBalance) {
+        super("Amount exceeds balance by " + negativeBalance);
+        this.negativeBalance = negativeBalance;
+
+        try (PrintWriter writer = new PrintWriter(new FileWriter("logfile.txt", true))) {
+            writer.println("Amount exceeds balance by " + negativeBalance);
+        } catch (IOException e) {
+            System.err.println("Failed to write to logfile: " + e.getMessage());
+        }
     }
 }
